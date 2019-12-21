ed331ebd3a44e3ec3d19cbb4f2f67ad5a5f16063
Nicola Di Marco <dimarco.nicola@gmail.com>
2019-12-21 12:22:33 +0100

    reduce logo size

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from '../logo.svg';
+import logo from '../logo.png';
 
 const LogoContainer = styled.img`
 
   @media (max-width: 768px) {
+    height: 30px;
   }
 `;
 
