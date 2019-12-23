614eff7b3dae0826f00f3171e4709429ab2c3760
Nicola Di Marco <dimarco.nicola@gmail.com>
2019-12-23 18:04:29 +0100

    gradient component content

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import Hero from '../Components/Hero';
-import ComparisonTable from '../Components/ComparisonTable';
-import GradienBgSection from '../Components/GradienBgSection';
-import GradientText from '../Components/GradientText';
-import Features from '../Components/Features';
-import BlackBGgradientText from '../Components/BlackBGgradientText';
-import BigBlackTitle from '../Components/BigBlackTitle';
-import FAQ from '../Components/FAQ';
-import Newsletter from '../Components/Newsletter';
-
-
+import React from "react";
+import Hero from "../Components/Hero";
+import ComparisonTable from "../Components/ComparisonTable";
+import GradienBgSection from "../Components/GradienBgSection";
+import GradientText from "../Components/GradientText";
+import Features from "../Components/Features";
+import BlackBGgradientText from "../Components/BlackBGgradientText";
+import BigBlackTitle from "../Components/BigBlackTitle";
+import FAQ from "../Components/FAQ";
+import Newsletter from "../Components/Newsletter";
 
 function Home() {
   return (
     <div>
-      <Hero/>
-      <ComparisonTable/>
-      <GradienBgSection/>
-      <FAQ/>
-      <GradientText/>
-      <Newsletter/>
-      <Features/>
-      <BlackBGgradientText/>
-      <BigBlackTitle/>
-      <Newsletter/>
+      <Hero />
+      <ComparisonTable />
+      <GradienBgSection />
+      <FAQ />
+      <GradientText GradientTextContent="3% better performing than the top portfolios in the World*. How about that?" />
+      <Newsletter />
+      <Features />
+      <BlackBGgradientText />
+      <BigBlackTitle />
+      <Newsletter />
     </div>
   );
 }
