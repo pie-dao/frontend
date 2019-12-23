4c9f8abc166d73b4e8a400ce3f1996fe835aa51f
Nicola Di Marco <dimarco.nicola@gmail.com>
2019-12-23 18:06:42 +0100

    starting to update the investment page

--- src/Pages/InvestmentDetail.js
+++ src/Pages/InvestmentDetail.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import TopNavi from '../Components/TopNavi';
-import Hero from '../Components/Hero';
-import ComparisonTable from '../Components/ComparisonTable';
-import GradienBgSection from '../Components/GradienBgSection';
-import GradientText from '../Components/GradientText';
-import Features from '../Components/Features';
-import BlackBGgradientText from '../Components/BlackBGgradientText';
-import BigBlackTitle from '../Components/BigBlackTitle';
-import Newsletter from '../Components/Newsletter';
+import React from "react";
+import ComparisonTable from "../Components/ComparisonTable";
+import GradientText from "../Components/GradientText";
+import Features from "../Components/Features";
+import BlackBGgradientText from "../Components/BlackBGgradientText";
+import BigBlackTitle from "../Components/BigBlackTitle";
+import Newsletter from "../Components/Newsletter";
+import AWPDetail from "../Components/AWPDetail";
 
 function InvestmentDetail() {
   return (
     <div className="App">
-      <Hero/>
-      <ComparisonTable/>
-      <GradientText/>
-      <Newsletter/>
-      <Features/>
-      <BlackBGgradientText/>
-      <BigBlackTitle/>
-      <Newsletter/>
+      <GradientText GradientTextContent="You will gain way more"/>
+      <AWPDetail />
+      <ComparisonTable />
+      <Newsletter />
+      <Features />
+      <BlackBGgradientText />
+      <BigBlackTitle />
+      <Newsletter />
     </div>
   );
 }
