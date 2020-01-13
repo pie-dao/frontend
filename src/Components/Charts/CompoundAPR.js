786bcf085b4494f951e6863a8fb95dc43a0dcc49
Mick de Graaf <mick@mickdegraaf.nl>
2020-01-13 17:12:12 -0500

    refactored CompoundAPR to functional component

--- src/Components/Charts/CompoundAPR.js
+++ src/Components/Charts/CompoundAPR.js
@@ -1,7 +1,6 @@
-import React,{ Component } from 'react';
+import React,{ useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import styled from "styled-components";
-
 
 const Placeholder = styled.div`
   margin: 5px 30px 20px 5px;
@@ -9,59 +8,40 @@ const Placeholder = styled.div`
   height: 300px;
 `;
 
-class CompoundAPR extends Component {
 
-  state = {
-    comparisonData: null,
-  }
-  componentDidMount(){
-    this.getData();
-  }
+const CompoundAPR = props => {
+  const [comparisonData, setComparisonData] = useState(null);
+  const [error, setError] = useState(null);
 
-
-
-  
-  async getData() {
+  async function getData() {
     const res = await fetch(`https://pie-protocol-api.herokuapp.com/charts/comparison/2019-12`);
-    //const res = await fetch(`http://localhost:3999/charts/comparison/2019-12`);
-    
-    
-    const data = await res.json();
-    this.setState({
-      comparisonData: data
-    })
+    res
+      .json()
+      .then(res => setComparisonData(res))
+      .catch(err => setError(err));
+
   }
 
+  useEffect(() => {
+    getData();
+  });
 
-  renderComparisonChart() {
-    const {comparisonData} = this.state;
-
-
-    return (
-      <div>
-        {!(comparisonData) ? <Placeholder/> :
-          <LineChart width={600} height={380} data={comparisonData} margin={{top: 0, right: 30, left: 20, bottom: 5}}>
-            <XAxis dataKey="month"/>
-            <YAxis domain={[0, 40]}/>
-            <CartesianGrid strokeDasharray="3 3"/>
-            <Tooltip/>
-            <Legend />
-            <Line type="monotone" dataKey="awp" stroke="#00C49F" activeDot={{r: 8}}/>
-            <Line type="monotone" dataKey="awpPlus" stroke="#EC774C" activeDot={{r: 8}}/>
-            <Line type="monotone" dataKey="compound" stroke="#0088FE" activeDot={{r: 8}}/>
-          </LineChart>
-        }
-      </div>
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        {this.renderComparisonChart()}
-      </div>
-    );
-  }
+  return (
+    <div>
+      {!(comparisonData) ? <Placeholder/> :
+        <LineChart width={600} height={380} data={comparisonData} margin={{top: 0, right: 30, left: 20, bottom: 5}}>
+          <XAxis dataKey="month"/>
+          <YAxis domain={[0, 40]}/>
+          <CartesianGrid strokeDasharray="3 3"/>
+          <Tooltip/>
+          <Legend />
+          <Line type="monotone" dataKey="awp" stroke="#00C49F" activeDot={{r: 8}}/>
+          <Line type="monotone" dataKey="awpPlus" stroke="#EC774C" activeDot={{r: 8}}/>
+          <Line type="monotone" dataKey="compound" stroke="#0088FE" activeDot={{r: 8}}/>
+        </LineChart>
+      }
+    </div>
+  );
 }
 
 export default CompoundAPR;
