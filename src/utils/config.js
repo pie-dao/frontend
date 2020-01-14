5f89563f50cbefda95420f8405f74263082e0a0b
Nicola Di Marco <dimarco.nicola@gmail.com>
2020-01-14 15:50:30 -0500

    fixing wrong url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,19 +1,15 @@
 module.exports = {
     DEBUG: process.env.NODE_ENV === 'development',
-    titlePrivacy: 'Privacy',
-    titlePro: 'Dexwallet PRO',
-    titleTerms: 'Terms & Conditions',
-    titleWhitelabel: 'Dexwallet Whitelabel',
-    titleBuy: 'Buy Cryptocurrencies',
-    titleDiscord: 'Dexwallet Discord Channel',
+    titleBuy: 'Buy Pie Token',
+    titleDiscord: 'Pie Network Discord Channel',
   
     siteName: 'Pie - Top investment for everyone',
     author: 'Pie Network',
     description:
       'The richest people in the World have gained 10% every year for the past 10 years. We think it is fair for you to make some real money too.',
     DiscordDescription: 'Redirecting to Pie Network Discord Channel',
-    siteUrl: 'https://www.pie.dexwallet.io/',
-    image: 'https://www.pie.dexwallet.io/img/preview.jpg',
+    siteUrl: 'https://www.pie.network/',
+    image: 'https://www.pie.network/img/preview.jpg',
   
     twitter: '@Dexwallet',
     fbUserId: '@NAME-HERE',
