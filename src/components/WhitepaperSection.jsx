a86eb47b67597bbda446a1019080b9ec6c06b7f4
Nicola Di Marco <dimarco.nicola@gmail.com>
2020-01-27 17:38:30 -0500

    newsletter

--- src/components/WhitepaperSection.jsx
+++ src/components/WhitepaperSection.jsx
@@ -5,9 +5,27 @@ import { view } from 'react-easy-state';
 
 const WhitepaperSection = () => {
   return (
-    <>
-      WhitepaperSection
-    </>
+    <div className="whitepaper-container">
+      <div>
+        Fancy a piece of pie
+        <span role="img" aria-label="cake">
+          &nbsp;🍰
+        </span>
+        ? Request the Whitepaper
+      </div>
+      <a
+        className="whitepaper-link lg:text-quiteverybig"
+        href="https://pie283460.typeform.com/to/uy9NZt"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="whitepaper-icon">
+          <span role="img" aria-label="document">
+            📄
+          </span>
+        </div>
+      </a>
+    </div>
   );
 };
 
