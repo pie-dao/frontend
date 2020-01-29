4bac69c857e884cf40e59d835321e7c498af8671
Dan Matthews <dan@cryptobuilders.io>
2020-01-29 12:47:33 -0500

    attempt to fix netlify build error

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 module.exports = {
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, 'build'),
     compress: true,
     historyApiFallback: true,
     port: 5000,
