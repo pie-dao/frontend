0feffd8b587833519fddae5a6e6336a7010c79ab
Nicola Di Marco <dimarco.nicola@gmail.com>
2020-01-29 12:33:57 -0500

    mobile menu

--- src/components/TopNavigation.jsx
+++ src/components/TopNavigation.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
-import { view } from 'react-easy-state';
+import { store, view } from 'react-easy-state';
 
 import ConnectWeb3Button from './ConnectWeb3Button';
+
+const topNavigationStore = store({
+  toggle: () => {
+    topNavigationStore.mobileMenuVisible = !topNavigationStore.mobileMenuVisible;
+  },
+  mobileMenuVisible: false,
+});
+
 
 const TopNavigation = ({ links, images }) => {
   const {
@@ -15,6 +23,7 @@ const TopNavigation = ({ links, images }) => {
   } = links;
   const { logo } = images;
 
+  const { mobileMenuVisible } = topNavigationStore;
   return (
     <div className="nav-container">
       <div className="left">
@@ -23,24 +32,91 @@ const TopNavigation = ({ links, images }) => {
         </Link>
       </div>
       <div className="right">
-        <Link to="/portfolio" className="link" onClick={portfolio}>
+        <Link
+          to="/portfolio"
+          className="link hidden lg:block"
+          onClick={portfolio}
+        >
           portfolio
         </Link>
-        <Link to="/mint-redeem" className="link" onClick={mintredeem}>
+        <Link
+          to="/mint-redeem"
+          className="link hidden lg:block"
+          onClick={mintredeem}
+        >
           mint/redeem
         </Link>
         <a
           onClick={whitepaper}
-          className="link"
+          className="link hidden lg:block"
           href="https://pie283460.typeform.com/to/uy9NZt"
           target="_blank"
           rel="noopener noreferrer"
         >
           whitepaper
         </a>
-        <div className=".hidden lg:block" />
+        <div className="hidden lg:block" />
 
-        <ConnectWeb3Button />
+        <ConnectWeb3Button className="hidden lg:block" />
+
+        <button
+          className="hamburger"
+          id="trigger-overlay"
+          type="button"
+          onClick={topNavigationStore.toggle}
+        >
+          <img src="./assets/img/hamburgerIcon.svg" height="19" alt="hamburger icon" />
+        </button>
+        {mobileMenuVisible && (
+          <div className="overlay overlay-hugeinc open">
+            <button
+              type="button"
+              className="overlay-close"
+              onClick={topNavigationStore.toggle}
+            >
+              Close
+            </button>
+            <nav>
+              <ul>
+                <li>
+                  <Link
+                    onClick={topNavigationStore.toggle}
+                    to="/"
+                  >
+                    home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/portfolio"
+                    onClick={topNavigationStore.toggle}
+                  >
+                    portfolio
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/mint-redeem"
+                    onClick={topNavigationStore.toggle}
+                  >
+                    mint/redeem
+                  </Link>
+                </li>
+                <li>
+                  <a
+                    onClick={topNavigationStore.toggle}
+                    className="navbar-item"
+                    href="https://pie283460.typeform.com/to/uy9NZt"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    whitepaper
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        )}
       </div>
     </div>
   );
