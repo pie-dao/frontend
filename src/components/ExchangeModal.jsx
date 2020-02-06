c6a41616a9a4e86fd6bc9f2925cc133e82094290
Nicola Di Marco <dimarco.nicola@gmail.com>
2020-02-06 16:21:56 -0500

    modal responsive

--- src/components/ExchangeModal.jsx
+++ src/components/ExchangeModal.jsx
@@ -21,7 +21,7 @@ const ExchangeModal = () => {
       className="exchange-modal"
       isActive={exchangeModal.isActive}
       closeModal={exchangeModal.close}
-      showAnimation={false}
+      showAnimation="true"
       modalBoxStyle={{
         width: '90%',
         maxWidth: 600,
