ee41db81236089c8cc105e28c865efc52040673e
Nicola Di Marco <dimarco.nicola@gmail.com>
2020-02-10 15:39:19 -0500

    tracking events on mixpanel

--- src/components/ExchangeModal.jsx
+++ src/components/ExchangeModal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
 import { Modal } from 'minimal-react-modal';
 import { view } from 'react-easy-state';
+import PropTypes from 'prop-types';
 
 import amountFormatter from '../utils/amountFormatter';
 import ConnectWeb3Button from './ConnectWeb3Button';
@@ -11,7 +11,7 @@ import If from './If';
 import myAccount from '../stores/myAccount';
 import Unless from './Unless';
 
-const ExchangeModal = () => {
+const ExchangeModal = ({ mixpanel }) => {
   const { account } = eth;
 
   const airdropRequired = myAccount.airdropRequired();
@@ -158,7 +158,14 @@ const ExchangeModal = () => {
                 type="button"
                 className="btn btn-primary"
                 disabled={exchangeModal.inputError}
-                onClick={exchangeModal.buy}
+                onClick={() => {
+                  exchangeModal.buy();
+                  mixpanel.cta({
+                    position: 'AWP++ Buy Modal',
+                    type: 'button',
+                    label: 'Buy',
+                  });
+                }}
               >
                 Buy
               </button>
@@ -179,6 +186,15 @@ const ExchangeModal = () => {
       </a>
     </Modal>
   );
+};
+
+ExchangeModal.propTypes = {
+  mixpanel: PropTypes.shape({
+    cta: PropTypes.func.isRequired,
+  }).isRequired,
+  links: PropTypes.shape({
+    portfolio: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default view(ExchangeModal);
