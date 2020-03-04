31033771799b8d9ae410747124a6731a14c4f494
Dan Matthews <dan@cryptobuilders.io>
2020-03-04 14:03:29 +0200

    removed password gate (#110)
    * removed password gate
    
    * Reconstructing landing
    
    * Fixing numbers
    
    * Updating FAQ
    
    * Updating FAQ
    
    * Rm trailing space
    
    * Fixing typos
    
    Co-authored-by: Alessio Delmonti <alessio.d@gmail.com>
    Co-authored-by: Mick de Graaf <mick@mickdegraaf.nl>

--- src/components/AddRemoveLiquidity.jsx
+++ src/components/AddRemoveLiquidity.jsx
@@ -4,7 +4,6 @@ import { view } from 'react-easy-state';
 import Slider from 'rc-slider';
 import PropTypes from 'prop-types';
 
-import PasswordGate from './PasswordGate';
 import addRemoveLiquidity from '../stores/addRemoveLiquidity';
 import AwpLight from './charts/AwpLight';
 import SingleAsset from './SingleAsset';
@@ -33,100 +32,98 @@ const AddRemoveLiquidity = ({ mixpanel }) => {
   const max = sliderMax();
 
   return (
-    <PasswordGate>
-      <section className="content">
-        <div className="liquidity-container lg:flex-row lg:w-94pc">
-          <div className="liquidity-left lg:w-auto lg:m-0 lg:min-w-300px">
-            <div className="tab-navi">
-              {/* <div
-                className={tab === 'add' ? 'tab-item active' : 'tab-item'}
-                onClick={() => {
-                  // selectAdd();
-                  mixpanel.cta({
-                    position: 'Mint-Redeem',
-                    type: 'tab navi',
-                    label: 'Mint',
-                  });
-                }}
-              >
-                Mint AWP++
-              </div> */}
-              <div
-                className={tab === 'remove' ? 'tab-item active' : 'tab-item'}
-                onClick={() => {
-                  selectRemove();
-                  mixpanel.cta({
-                    position: 'Mint-Redeem',
-                    type: 'tab navi',
-                    label: 'Redeem',
-                  });
-                }}
-              >
-                Redeem AWP++
-              </div>
-            </div>
-            <div className="liquidity-amount lg:text-liquidity-amount">
-              {slider[tab]}
-            </div>
-            <div className="awp-label lg:text-big lg:leading-none lg:m-0">AWP++</div>
-            <div className="slider-wrapper">
-              <Slider
-                min={0}
-                max={max}
-                step={0.1}
-                onChange={addRemoveLiquidity.sliderChange}
-                value={slider[tab]}
-                vertical={false}
-              />
-            </div>
-            <button
+    <section className="content">
+      <div className="liquidity-container lg:flex-row lg:w-94pc">
+        <div className="liquidity-left lg:w-auto lg:m-0 lg:min-w-300px">
+          <div className="tab-navi">
+            {/* <div
+              className={tab === 'add' ? 'tab-item active' : 'tab-item'}
               onClick={() => {
-                buttonClick();
+                // selectAdd();
+                mixpanel.cta({
+                  position: 'Mint-Redeem',
+                  type: 'tab navi',
+                  label: 'Mint',
+                });
+              }}
+            >
+              Mint AWP++
+            </div> */}
+            <div
+              className={tab === 'remove' ? 'tab-item active' : 'tab-item'}
+              onClick={() => {
+                selectRemove();
                 mixpanel.cta({
                   position: 'Mint-Redeem',
                   type: 'tab navi',
                   label: 'Redeem',
                 });
               }}
-              type="button"
-              className="btn"
             >
-              {tab === 'add' ? 'Add ' : 'Remove '}
-              Liquidity
-            </button>
+              Redeem AWP++
+            </div>
           </div>
-          <div className="liquidity-right lg:w-auto lg:ml-2pc lg:tex-left lg:flex-row lg:flex-grow">
-            <div className="liquidity-column lg:items-start lg:text-left lg:pl-3pc lg:pr-1pc lg:max-w-300px">
-              <h1 className="title lg:text-left lg:text-big py-2pc">Liquidity Breakdown</h1>
-              <AwpLight />
-              <div className="normal-text lg:m-0 lg:text-left py-2pc">
-              We backtested the All Weather Portfolio and the AWP++ against DeFi
-              Landing in the last 12 months. Don&apos;t take our word for it. Have a
-              look at the chart on the left.
-              </div>
+          <div className="liquidity-amount lg:text-liquidity-amount">
+            {slider[tab]}
+          </div>
+          <div className="awp-label lg:text-big lg:leading-none lg:m-0">AWP++</div>
+          <div className="slider-wrapper">
+            <Slider
+              min={0}
+              max={max}
+              step={0.1}
+              onChange={addRemoveLiquidity.sliderChange}
+              value={slider[tab]}
+              vertical={false}
+            />
+          </div>
+          <button
+            onClick={() => {
+              buttonClick();
+              mixpanel.cta({
+                position: 'Mint-Redeem',
+                type: 'tab navi',
+                label: 'Redeem',
+              });
+            }}
+            type="button"
+            className="btn"
+          >
+            {tab === 'add' ? 'Add ' : 'Remove '}
+            Liquidity
+          </button>
+        </div>
+        <div className="liquidity-right lg:w-auto lg:ml-2pc lg:tex-left lg:flex-row lg:flex-grow">
+          <div className="liquidity-column lg:items-start lg:text-left lg:pl-3pc lg:pr-1pc lg:max-w-300px">
+            <h1 className="title lg:text-left lg:text-big py-2pc">Liquidity Breakdown</h1>
+            <AwpLight />
+            <div className="normal-text lg:m-0 lg:text-left py-2pc">
+            We backtested the All Weather Portfolio and the AWP++ against DeFi
+            Landing in the last 12 months. Don&apos;t take our word for it. Have a
+            look at the chart on the left.
             </div>
-            <div className="liquidity-column lg:flex-grow lg:pl-2pc lg:pr-3pc">
-              <SingleAsset token="tlt" />
-              <SingleAsset token="vti" />
-              <SingleAsset token="iei" />
-              <SingleAsset token="gld" />
-              <SingleAsset token="gsg" />
+          </div>
+          <div className="liquidity-column lg:flex-grow lg:pl-2pc lg:pr-3pc">
+            <SingleAsset token="tlt" />
+            <SingleAsset token="vti" />
+            <SingleAsset token="iei" />
+            <SingleAsset token="gld" />
+            <SingleAsset token="gsg" />
 
-              <SingleAsset token="btc" />
-              <SingleAsset token="eth" />
-              <SingleAsset token="link" />
-              <SingleAsset token="zrk" />
-              <SingleAsset token="snx" />
-              <SingleAsset token="ren" />
-              <SingleAsset token="lrc" />
-              <SingleAsset token="knc" />
-              <SingleAsset token="bnt" />
-              <SingleAsset token="mln" />
-            </div>
+            <SingleAsset token="btc" />
+            <SingleAsset token="eth" />
+            <SingleAsset token="link" />
+            <SingleAsset token="zrk" />
+            <SingleAsset token="snx" />
+            <SingleAsset token="ren" />
+            <SingleAsset token="lrc" />
+            <SingleAsset token="knc" />
+            <SingleAsset token="bnt" />
+            <SingleAsset token="mln" />
           </div>
         </div>
-      </section>
-    </PasswordGate>
+      </div>
+    </section>
   );
 };
 
